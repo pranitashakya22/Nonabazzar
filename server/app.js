d47dc35b714defefebd2a8054a4222cf62d08d21
Pranita Shakya <pranitashakya194@gmail.com>
2025-01-23 20:22:34 +0545

    shop-contact-cart page

--- server/app.js
+++ server/app.js
@@ -16,8 +16,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => {?
-    
+.then(() => {
     console.log('Database Connection is ready....');
 
     // Server
